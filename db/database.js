d331e3ea7e1011262c0d7215c53b9890b518f79b
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-03-18 15:02:51 -0400

    working on database setup

---
+++ db/database.js
@@ -0,0 +1,20 @@
+// get the client
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'test'
+});
+
+connection.query(
+  'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+  function(err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const connection = require("./db/database")
 
 // function that on start will give the options to : view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 
