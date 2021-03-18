f72aa6154f88b25a14700aa499904dfe23f69f54
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-03-18 00:15:35 -0400

    pseudo code added to server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const inquirer = require("inquirer");
+
+// function that on start will give the options to : view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+
+// function that on view all departments will present a formatted table showing department names and department ids when
+
+// function that on view all roles will present the job title, role id, the department that role belongs to, and the salary for that role
+
+// function that on view all employees will present a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+
+// function that will on add a department prompt user to enter the name of the department and that department is added to the database
+
+// function that will on add a department prompt user to enter the name, salary, and department for the role and that role is added to the database
+
+// function that will on add an employee prompt user to enter the employee’s first name, last name, role, and manager and that employee is added to the database
+
+// function that will on update employee prompt user to to update and their new role and this information is updated in the database 
