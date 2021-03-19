f87fba23c017ae7f209bd8083db045ae85f24020
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-03-18 21:43:56 -0400

    working on seeds and schema

---
+++ db/seed.sql
@@ -0,0 +1,12 @@
+use employees
+
+INSERT INTO department (name)
+VALUES
+
+
+INSERT INTO department (title, salary, department_id)
+VALUES
+
+
+INSERT INTO department (first_name, last_name, role_id, manager_id)
+VALUES
